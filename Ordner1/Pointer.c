87aded292814f4c4c135dff300b9e68bf1e84888
NiklasTFE23 <156666252+NiklasTFE23@users.noreply.github.com>
2024-02-12 11:23:47 +0100

    1

--- Ordner1/Pointer.c
+++ Ordner1/Pointer.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    int a = 100;
-    printf ("%i\n",a);
-    int *pa = &a;
-    *pa = 20;
-    printf ("%i\n",a);
-    printf ("%p\n",pa);
+    int aaa = 100;
+    printf ("%i\n",aaa);
+    int *paa = &aaa;
+    *paa = 20;
+    printf ("%i\n",aaa);
+    printf ("%p\n",paa);
 
-    printf ("Size of Char: %u\n", sizeof(char*));
+    printf ("Size of Char: %lu\n", sizeof(char*));
 
     return 0;
 }
