0aae6c739fc762e4ab1f4b73ee74a4c8c4a9362d
NiklasTFE23 <156666252+NiklasTFE23@users.noreply.github.com>
2024-02-22 12:04:49 +0100

    2.

---
+++ CMAKE/build/CMake.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+ printf ("Hallo World");
+ int A =3;
+ int B =3;
+ int C = A/B;
+ printf ("Der Wert von C ist %i",C);   
+ return 0;
+}
---
+++ CMAKE/run_cmake.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cmake -S . -B build -CMAKE_BUILD_TYPE=DEBUG
