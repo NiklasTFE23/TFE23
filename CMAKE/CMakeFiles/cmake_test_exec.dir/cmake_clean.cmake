2fc3e62680d6864cbbd0c823aaa84e1d336ff1e3
NiklasTFE23 <156666252+NiklasTFE23@users.noreply.github.com>
2024-02-22 10:15:46 +0100

    1.

---
+++ CMAKE/CMakeFiles/cmake_test_exec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_test_exec.dir/CMake.c.o"
+  "CMakeFiles/cmake_test_exec.dir/CMake.c.o.d"
+  "cmake_test_exec"
+  "cmake_test_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmake_test_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
