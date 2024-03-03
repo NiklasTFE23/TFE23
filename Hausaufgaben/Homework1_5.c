bff10ff7918ea74bb141cdd2de4ecb3a5d7d9811
NiklasTFE23 <156666252+NiklasTFE23@users.noreply.github.com>
2024-03-04 00:30:06 +0100

    1

--- Hausaufgaben/Homework1_5.c
+++ Hausaufgaben/Homework1_5.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
-#include <math.h>
 
-int main()
-{
-    unsigned long long Decimal=0;
-    unsigned long long Var = 0;
-    int i = 0;
-    printf("Gib eine ganze Zahl ein: ");
-    scanf("%lld",&Decimal);
+// Funktion zur Berechnung der Binärdarstellung einer Dezimalzahl
+void printBinary(unsigned long long decimal) {
+    // Iteration über die Bitpositionen
+    for (int i = 63; i >= 0; i--) {
+        // Berechnung der aktuellen Potenz von 2
+        unsigned long long powerOfTwo = 1ULL << i;
+
+        // Überprüfen, ob die aktuelle Potenz von 2 in die Dezimalzahl passt
+        if (decimal >= powerOfTwo) {
+            printf("1");
+            decimal -= powerOfTwo; // Abziehen der aktuellen Potenz von 2 von der Dezimalzahl
+        } else {
+            printf("0");
+        }
+    }
+}
+
+int main() {
+    unsigned long long decimal = 0;
+A:
+    // Eingabe der Dezimalzahl
+    printf("Gib eine ganze Zahl zwischen 0 und 18.446.744.073.709.551.615 ein: ");
+    scanf("%llu", &decimal);
     printf("------------------------------\n");
 
-for (i=63;i>=0;i--)
+if (decimal<=0||decimal>=18446744073709551615)
 {
-   unsigned long long temp = 2^i;
-Var=Decimal/temp;
-if (Var==1)
-{
-    printf("1");
-    Decimal= Decimal-(2^i);
-}
-else
-{
-    printf("0");
-}
+    printf("Ungültig\n------------------------------\n");
+
+    goto A;
 }
 
-return 0;
+    // Ausgabe der Binärdarstellung
+    printf("Binärdarstellung: ");
+    printBinary(decimal);
+    printf("\n");
+
+    return 0;
 }
